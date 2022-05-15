aeb9d1a349c038b42215b6fdc655fb45c5dc401e
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-15 12:28:01 +0430

    Use suspense to render a loading fallback

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -2,27 +2,17 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {fetchPokemon, PokemonDataView} from '../pokemon'
+import { fetchPokemon, PokemonDataView } from '../pokemon'
 
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-
-// 🐨 create a variable called "pokemon" (using let)
 let pokemon: any = null
 const pokemonPromise = fetchPokemon('pikachu')
 pokemonPromise.then(data => {
   pokemon = data
 })
 
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
-
 function PokemonInfo() {
   if (!pokemon) {
+    console.log('Gonna throw')
     throw pokemonPromise
   }
 
@@ -42,7 +32,7 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback={() => 'loading...'}>
+        <React.Suspense fallback={'loading...'}>
           <PokemonInfo />
         </React.Suspense>
       </div>
