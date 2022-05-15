61876a335b678e86585eab00261a9b19b26ffb02
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-15 14:17:37 +0430

    Create 'createResource' resouce factory

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -4,26 +4,41 @@
 import * as React from 'react'
 import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
 
-let pokemon: any = null
-let pokemonError: Error | null = null
-const pokemonPromise = fetchPokemon('pikacha')
-pokemonPromise
-  .then(data => {
-    pokemon = data
-  })
-  .catch(err => {
-    pokemonError = err
-  })
+function createResource<ResourceType>(promise: Promise<ResourceType>) {
+  let data: ResourceType | null = null
+  let fetchErr: Error | null = null
+
+  let status: 'pending' | 'resolved' | 'rejected' = 'pending'
+
+  promise.then(
+    dt => {
+      status = 'resolved'
+      data = dt
+    },
+    err => {
+      status = 'rejected'
+      fetchErr = err
+    },
+  )
+
+  return {
+    read() {
+      switch (status) {
+        case 'pending':
+          throw promise
+        case 'rejected':
+          throw fetchErr
+        default:
+          return data
+      }
+    },
+  }
+}
+
+const pokemonResource = createResource(fetchPokemon('Pikachu'))
 
 function PokemonInfo() {
-  if (pokemonError) throw pokemonError
-  if (!pokemon) {
-    console.log('Gonna throw')
-    throw pokemonPromise
-  }
-
-  console.log(pokemon)
-
+  const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
