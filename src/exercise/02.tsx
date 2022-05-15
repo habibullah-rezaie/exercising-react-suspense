0210b2ec1a4d7f6a3af0c0c4989e8f421caf5432
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-15 15:05:38 +0430

    Get 'PokemonInfo' showing with suspense

---
+++ src/exercise/02.tsx
@@ -0,0 +1,67 @@
+// Render as you fetch
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+import {
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonForm,
+  PokemonDataView,
+  PokemonErrorBoundary,
+} from '../pokemon'
+import {createResource} from '../utils'
+
+function PokemonInfo({
+  pokemonResource,
+}: {
+  pokemonResource: ReturnType<typeof createResource>
+}) {
+  const pokemon = pokemonResource.read()
+
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+  const [pokemonResource, setPokemonResource] = React.useState<ReturnType<
+    typeof createResource
+  > | null>(null)
+
+  React.useEffect(() => {
+    if (!pokemonName) setPokemonResource(null)
+    else setPokemonResource(createResource(fetchPokemon(pokemonName)))
+  }, [pokemonName])
+
+  function handleSubmit(newPokemonName: string) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        {pokemonResource ? (
+          <PokemonErrorBoundary>
+            <React.Suspense
+              fallback={<>{<PokemonInfoFallback name={pokemonName} />}</>}
+            >
+              <PokemonInfo pokemonResource={pokemonResource} />
+            </React.Suspense>
+          </PokemonErrorBoundary>
+        ) : (
+          'Submit a pokemon'
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
