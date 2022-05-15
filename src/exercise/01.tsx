94c3eb13bb8172643fe1e4d9b6436d51d3dbd107
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-15 12:13:52 +0430

    Use TypeScript

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -3,21 +3,16 @@
 
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
+import {fetchPokemon, PokemonDataView} from '../pokemon'
 
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 // 🐨 create a variable called "pokemon" (using let)
-
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
+let pokemon: any = null
+const pokemonPromise = fetchPokemon('pikachu')
+pokemonPromise.then(data => {
+  pokemon = data
+})
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
@@ -27,11 +22,12 @@ const pokemon = {
 // 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (!pokemon) {
+    throw pokemonPromise
+  }
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
+  console.log(pokemon)
+
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -46,8 +42,9 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
+        <React.Suspense fallback={() => 'loading...'}>
+          <PokemonInfo />
+        </React.Suspense>
       </div>
     </div>
   )
